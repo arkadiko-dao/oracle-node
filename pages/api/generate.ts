4436df43b905a952c9cad816abc5099e4ce165f5
Niel Deckx <hello@nieldeckx.be>
2022-11-15 08:52:17 +0800

    endpoint to generate key pair

---
+++ pages/api/generate.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import secp256k1 from 'secp256k1';
+import { randomBytes } from 'crypto';
+
+type Data = {
+  privateKey: string,
+  publicKey: string
+}
+
+type DataError = {
+  error: string,
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | DataError>
+) {
+  let privateKey
+  do {
+    privateKey = randomBytes(32)
+  } while (!secp256k1.privateKeyVerify(privateKey))
+  const privateKeyString = privateKey.toString("hex");
+
+  // get the public key in a compressed format
+  const publicKey = secp256k1.publicKeyCreate(privateKey)
+  const publicKeyString = Buffer.from(publicKey).toString("hex");
+
+  res.status(200).json({ privateKey: privateKeyString, publicKey: publicKeyString })
+}
