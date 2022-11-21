c588e23e491f9fa0eb38209a2e52a4e49ecd42a8
Niel Deckx <hello@nieldeckx.be>
2022-11-21 10:57:18 +0800

    feat: new sources (#7)

---
+++ sources/coinbase.ts
@@ -0,0 +1,33 @@
+import { tokenDecimals } from "@common/config";
+import { fetchPriceAMM } from "./amm";
+import { PriceSourceInterface } from "./interface";
+
+export class SourceCoinbase implements PriceSourceInterface {
+
+  // Return price as int
+  public async fetchPrice(symbol: string): Promise<number> {
+    const price = await this.fetchPriceHelper(symbol);
+    return Math.round(price * Math.pow(10, tokenDecimals[symbol]));
+  }
+
+  // Return price as double
+  async fetchPriceHelper(symbol: string): Promise<number> {
+    // API
+    if (symbol == "STX") {
+      return await this.fetchPriceAPI("STX");
+    } else if (symbol == "BTC") {
+      return await this.fetchPriceAPI("BTC");
+    }
+
+    // AMM
+    const stxPrice = await this.fetchPriceAPI("STX");
+    return await fetchPriceAMM(symbol, stxPrice);
+  }
+
+  async fetchPriceAPI(id: string): Promise<number> {
+    const url = `https://api.exchange.coinbase.com/products/${id}-USD/ticker`;
+    const response = await fetch(url, { credentials: 'omit' });
+    const data = await response.json();
+    return data.price;
+  }
+}
