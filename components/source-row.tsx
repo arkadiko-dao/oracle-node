8c7f21749fa25d614ba9b2d345575291c3670ba5
Niel Deckx <hello@nieldeckx.be>
2023-06-16 10:55:37 +0800

    fix: dynamic symbols on index page

--- components/source-row.tsx
+++ components/source-row.tsx
@@ -3,16 +3,10 @@ import React from 'react';
 export type SourceRowObject = {
   source: string,
   currentNode: boolean,
-  stx: number,
-  btc: number,
-  usda: number,
-  usdaStx: number,
-  diko: number,
-  atAlex: number,
-  atAlexV2: number
+  prices: any
 }
 
-export default function SourceRow({ source, currentNode, stx, btc, usda, usdaStx, diko, atAlex, atAlexV2 }: SourceRowObject) {    
+export default function SourceRow({ source, currentNode, prices }: SourceRowObject) {    
   return (
     <tr key={source} className="bg-white">
       <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
@@ -23,27 +17,12 @@ export default function SourceRow({ source, currentNode, stx, btc, usda, usdaStx
           </span>
         ): null}
       </td>
-      <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
-        ${stx}
-      </td>
-      <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
-        ${btc}
-      </td>
-      <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
-        ${usda}
-      </td>
-      <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
-        ${usdaStx}
-      </td>
-      <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
-        ${diko}
-      </td>
-      <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
-        ${atAlex}
-      </td>
-      <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
-        ${atAlexV2}
-      </td>
+
+      {Object.keys(prices).map((symbol) => (
+        <th scope="col" className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+          {prices[symbol]}
+        </th>
+      ))}
     </tr>
   );
 };
