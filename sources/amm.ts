12c314bbce5357553ceb22034f1fc9215fb5c662
Niel Deckx <hello@nieldeckx.be>
2023-06-27 09:47:15 +0800

    fix: stx/usda price

--- sources/amm.ts
+++ sources/amm.ts
@@ -1,11 +1,13 @@
-import { getAutoAlexPrice, getAutoAlexV2Price, getUsdaPrice } from "./ammAlex";
-import { getDikoPrice } from "./ammArkadiko";
+import { getAutoAlexPrice, getAutoAlexV2Price, getUsdaPrice as getUsdaPriceAlex } from "./ammAlex";
+import { getDikoPrice, getUsdaPrice as getUsdaPriceArkadiko } from "./ammArkadiko";
 
 export async function fetchPriceAMM(symbol: string, stxPrice: number): Promise<number> {
   if (symbol == "DIKO") {
     return await getDikoPrice();
   } else if (symbol == "USDA") {
-    return await getUsdaPrice();
+    return await getUsdaPriceAlex();
+  } else if (symbol == "STX/USDA") {
+    return await getUsdaPriceArkadiko(stxPrice);
   } else if (symbol == "auto-alex") {
     return await getAutoAlexPrice(stxPrice);
   } else if (symbol == "auto-alex-v2") {
