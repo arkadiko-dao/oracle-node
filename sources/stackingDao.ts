22e19d6ef260bad1e719c53545f316b31a6ddece
Niel Deckx <hello@nieldeckx.be>
2025-01-21 09:46:13 +0100

    fix: use data-core-v2 for ststx

--- sources/stackingDao.ts
+++ sources/stackingDao.ts
@@ -1,17 +1,20 @@
-import { config } from '@common/config';
+import { config } from "@common/config";
 import {
   callReadOnlyFunction,
   cvToJSON,
-  contractPrincipalCV
-} from '@stacks/transactions';
+  contractPrincipalCV,
+} from "@stacks/transactions";
 
 export async function getStxPerStStx(): Promise<any> {
   const call = await callReadOnlyFunction({
     contractAddress: "SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG",
-    contractName: "stacking-dao-core-v1",
+    contractName: "data-core-v2",
     functionName: "get-stx-per-ststx",
     functionArgs: [
-      contractPrincipalCV("SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG", "reserve-v1"),
+      contractPrincipalCV(
+        "SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG",
+        "reserve-v1"
+      ),
     ],
     senderAddress: config.managerAddress as string,
     network: config.network,
