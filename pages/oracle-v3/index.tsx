d4d5b063c203028c80c7978668dadd9e1509320d
Niel Deckx <hello@nieldeckx.be>
2025-06-17 11:26:49 +0200

    fix: title

--- pages/oracle-v3/index.tsx
+++ pages/oracle-v3/index.tsx
@@ -50,7 +50,7 @@ export default function Home() {
           HEADER
         */}
         <h1 className="font-medium text-6xl text-center">
-          <a className="text-blue-600" href="https://arkadiko.finance/" target="_blank" rel="noreferrer">Arkadiko</a> Oracle V2.4
+          <a className="text-blue-600" href="https://arkadiko.finance/" target="_blank" rel="noreferrer">Arkadiko</a> Oracle V3.0
         </h1>
         <p className="mt-2 text-2xl text-gray-400 text-center">
           Prices from Pyth, DIA and StackingDAO.
